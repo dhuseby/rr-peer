2a52bebdbfa9948c8c60429147c4830ca042b941
Dave Grantham <dwg@linuxprogrammer.org>
2024-05-29 09:00:01 -0600

    add dial back
    Signed-off-by: Dave Grantham <dwg@linuxprogrammer.org>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
 use libp2p::{
+    core::ConnectedPoint,
     futures::{stream, StreamExt},
     identify,
     identity,
-    mdns,
     Multiaddr,
     PeerId,
     request_response::{self, ProtocolSupport},
     StreamProtocol,
-    swarm::{dial_opts::{DialOpts, PeerCondition}, NetworkBehaviour, SwarmEvent},
+    swarm::{/*dial_opts::{DialOpts, PeerCondition},*/ NetworkBehaviour, SwarmEvent},
 };
 use serde::{Serialize, Deserialize};
-use std::{collections::BTreeSet, error::Error};
+use std::{collections::BTreeMap, error::Error};
 use tokio::{select, time::{interval, Duration}};
 use tracing_subscriber::filter::EnvFilter;
 
 /// agent version
 const AGENT_VERSION: &'static str = "peer/0.0.1";
+const PROTOCOL: &'static str = "/foo/1";
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GreetRequest {
     message: String,
+    address: Multiaddr,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GreetResponse {
     message: String,
+    address: Multiaddr,
 }
 
 #[derive(NetworkBehaviour)]
 struct Behaviour {
     identify: identify::Behaviour,
     request_response: request_response::cbor::Behaviour<GreetRequest, GreetResponse>,
-    mdns: mdns::tokio::Behaviour,
 }
 
 #[tokio::main]
@@ -49,7 +51,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_quic()
         .with_behaviour(|key| Behaviour {
             identify: {
-                let cfg = identify::Config::new("/foo/bar/1".to_string(), key.public())
+                let cfg = identify::Config::new(PROTOCOL.to_string(), key.public())
                     .with_push_listen_addr_updates(true)
                     .with_agent_version(AGENT_VERSION.to_string());
                 identify::Behaviour::new(cfg)
@@ -58,89 +60,74 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 let cfg = request_response::Config::default()
                     .with_max_concurrent_streams(10);
                 request_response::cbor::Behaviour::<GreetRequest, GreetResponse>::new(
-                    [(StreamProtocol::new("/foo/1"), ProtocolSupport::Full)], cfg)
-            },
-            mdns: {
-                let cfg = mdns::Config::default();
-                mdns::tokio::Behaviour::new(cfg, key.public().to_peer_id()).unwrap()
+                    [(StreamProtocol::new(PROTOCOL), ProtocolSupport::Full)], cfg)
             },
         })?
         .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
         .build();
 
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+    }
+
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
 
-    // set up a timer to tick every 30 seconds
+    // set up a timer to tick every 10 seconds
     let mut timer = Box::pin(stream::unfold(interval(Duration::from_secs(10)), |mut interval| async {
         interval.tick().await;
         Some(((), interval))
     }));
 
-    let mut seen = Box::pin(BTreeSet::default());
-    let mut peers = Box::pin(BTreeSet::default());
+    let mut peers = Box::pin(BTreeMap::<PeerId, Multiaddr>::default());
+    let mut clients = Box::pin(BTreeMap::<PeerId, Multiaddr>::default());
     let mut my_addr = Box::pin(Multiaddr::empty());
 
     loop {
         select! {
             Some(_) = timer.next() => {
-                println!("Greeting Peers!");
                 let connected: Vec<PeerId> = swarm.connected_peers().cloned().collect();
+                println!("Greeting {} Peers!", connected.len());
                 for peer_id in &connected {
-                    if peers.contains(peer_id) {
-                        println!("Greeting: {peer_id}");
-                        swarm.behaviour_mut()
-                            .request_response
-                            .send_request(peer_id, GreetRequest { message: format!("Hello from {my_addr}") });
+                    match peers.get(peer_id) {
+                        Some(address) => {
+                            println!("Greeting: {peer_id}");
+                            swarm.behaviour_mut()
+                                .request_response
+                                .send_request(peer_id, GreetRequest { message: format!("Hello!!"), address: address.clone() });
+                        }
+                        None => {
+                            println!("Peer {peer_id} not in list of peers");
+                        }
                     }
                 }
             }
             event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(BehaviourEvent::RequestResponse(request_response::Event::Message { message, .. })) => match message {
+                SwarmEvent::Behaviour(BehaviourEvent::RequestResponse(request_response::Event::Message { peer, message })) => match message {
                     request_response::Message::Request { request, channel, .. } => {
                         let req: GreetRequest = request;
-                        println!("received request: {}", req.message);
+                        println!("Request from {}: {}", &req.address, req.message);
+                        let address = clients.get(&peer).unwrap_or(&Multiaddr::empty()).clone();
                         swarm.behaviour_mut()
                             .request_response
-                            .send_response(channel, GreetResponse { message: format!("Hello back from {my_addr}") })
+                            .send_response(channel, GreetResponse { message: format!("Hello Back!!"), address: address.clone() })
                             .expect("peer connection closed?");
+                        *my_addr = req.address;
+
+                        swarm.dial(address)?;
                     }
                     request_response::Message::Response { response, .. } => {
                         let resp: GreetResponse = response;
-                        println!("received response: {}", resp.message);
-                    }
-                }
-                SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-                    for (peer_id, multiaddr) in list {
-                        if peer_id != local_peer_id && seen.insert(peer_id) {
-                            println!("mDNS discovered a new peer: {peer_id}");
-                            let dial_opts = DialOpts::peer_id(peer_id)
-                                .condition(PeerCondition::DisconnectedAndNotDialing)
-                                .addresses(vec![multiaddr])
-                                .build();
-                            println!("Dialing {my_addr} -> {peer_id}");
-                            let _ = swarm.dial(dial_opts);
-                        }
-                    }
-                }
-                SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
-                    for (peer_id, _multiaddr) in list {
-                        if peer_id != local_peer_id {
-                            println!("mDNS peer has expired: {peer_id}");
-                            println!("Disconnecting from {peer_id}");
-                            seen.remove(&peer_id);
-                            peers.remove(&peer_id);
-                            swarm.disconnect_peer_id(peer_id).expect(&format!("failed to disconnect from {peer_id}"));
-                        }
+                        println!("Response from {}: {}", resp.address, resp.message);
+                        *my_addr = resp.address;
                     }
                 }
                 SwarmEvent::Behaviour(BehaviourEvent::Identify(identify::Event::Received { peer_id, info })) => {
                     if peer_id != local_peer_id {
                         if info.agent_version == AGENT_VERSION.to_string() {
-                            if peers.insert(peer_id) {
-                                println!("Found another peer: {peer_id}");
-                            }
+                            println!("Peer {peer_id} speaks our protocol");
                         } else {
-                            println!("{peer_id} doesn't speak {}", info.protocol_version);
+                            println!("{peer_id} doesn't speak our protocol");
                             println!("Disconnecting from {peer_id}");
                             peers.remove(&peer_id);
                             swarm.disconnect_peer_id(peer_id).expect(&format!("failed to disconnect from {peer_id}"));
@@ -149,6 +136,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 }
                 SwarmEvent::NewListenAddr { address, .. } => {
                     println!("Local peer is listening on {address}");
+                    *my_addr = address;
+                }
+                SwarmEvent::Dialing { peer_id, .. } => {
+                    if let Some(peer_id) = peer_id {
+                        println!("Dialing {peer_id}");
+                    }
+                }
+                SwarmEvent::ConnectionEstablished { peer_id, endpoint, .. } => {
+                    match endpoint {
+                        ConnectedPoint::Dialer { address, .. } => {
+                            println!("Successfully dialed to {peer_id}: {address}");
+                            peers.insert(peer_id, address.clone());
+                        }
+                        ConnectedPoint::Listener { send_back_addr, .. } => {
+                            clients.insert(peer_id, send_back_addr.clone());
+                            println!("Successfully received dial from {peer_id}: {send_back_addr}");
+                        }
+                    }
+                }
+                SwarmEvent::ConnectionClosed { peer_id, connection_id, .. } => {
+                    println!("Connection to {peer_id}:{connection_id} closed");
+                    clients.remove(&peer_id);
+                    peers.remove(&peer_id);
+                }
+                SwarmEvent::ExternalAddrConfirmed { address } => {
+                    println!("External address confirmed as {address}");
                     *my_addr = address;
                 }
                 _ => {}
